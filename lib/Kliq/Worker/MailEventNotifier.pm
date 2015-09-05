54eda6a547c493c9dadfd63e49460b25064ec0ab
Darren Duncan <darren@DarrenDuncan.net>
2015-09-04 22:30:14 -0700

    add HTML version of email body for notification of event creation

--- lib/Kliq/Worker/MailEventNotifier.pm
+++ lib/Kliq/Worker/MailEventNotifier.pm
@@ -21,13 +21,27 @@ sub work {
             . q{?id=com.tranzmt.app&referrer=}
             . q{contactId%3D}.$data->{contact_id}.q{%26eventId%3D}.$data->{event_id};
 
+        my $stream_url = q{rtmp://api.kliqmobile.com:1935/live/} . $data->{event_id};
+
         my $plaintext_body =
             $data->{sender} . qq{ wants to stream a live event to you.\n\n}
             . q{Event Title: } . $data->{title} . qq{\n}
             . q{Event Date: } . $data->{when_occurs} . qq{\n}
             . q{Event Location: } . $data->{location} . qq{\n\n}
-            . q{Live Stream at rtmp://api.kliqmobile.com:1935/live/} . $data->{event_id} . qq{\n\n}
+            . q{Live Stream at } . $stream_url . qq{\n\n}
             . q{Get the Kliq App at }.$store_url.qq{\n\n}
+            ;
+
+        my $htmltext_body =
+            qq{<html><body>\n}
+            . qq{<pre>\n}
+            . q{Event Title: } . $data->{title} . qq{\n}
+            . q{Event Date: } . $data->{when_occurs} . qq{\n}
+            . q{Event Location: } . $data->{location} . qq{\n\n}
+            . q{Live Stream at <a href="}.$stream_url.q{">}.$stream_url.qq{</a>\n\n}
+            . q{Get the Kliq App at <a href="}.$store_url.q{">}.$store_url.qq{</a>\n\n}
+            . qq{</pre>\n}
+            . qq{</body></html>\n}
             ;
 
         my $mail = Mail::Builder::Simple->new;
@@ -50,6 +64,7 @@ sub work {
             # reply => 'info@kliqmobile.com',
             subject => $data->{sender} . ' shared a live event stream with you',
             plaintext => $plaintext_body,
+            htmltext => $htmltext_body,
             priority => 1,
             mailer => 'KLIQ Mailer 0.01',
         );
