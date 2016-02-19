5b164b419e4b7521e64434030f5a95266d7063a8
Sachin Sebastian <sachinjsk@gmail.com>
2016-02-19 06:38:39 +0000

    Send push notifications to both android and ios

--- lib/Kliq/Worker/PushNotifier.pm
+++ lib/Kliq/Worker/PushNotifier.pm
@@ -32,13 +32,6 @@ sub work {
     $self->logger->info("Starting PushNotifier: " . Dumper($data));
 
     try {
-        my $username = $config->{bundle_id};
-        my $password = $config->{apikey};
-        my $carnival_base_uri = $config->{base_uri};
-
-        my $ua  = LWP::UserAgent->new;
-        my $uri = URI->new($carnival_base_uri . '/notifications');
-
         # Prepare push data
         my $request_hash = $data->{request};
         if (!$request_hash) {
@@ -49,19 +42,30 @@ sub work {
         die "Missing 'to' param in request" if (!$request_hash->{to});
         die "Missing 'payload' param in request" if (!$request_hash->{payload});
 
-        my $request_json = encode_json({ notification => $request_hash });
-        my $request = POST($uri, "Content-type" => "application/json", Accept => "application/json", Content => $request_json);
-        $request->authorization_basic($username, $password);
+        for my $os ('ios', 'android') {
+            my $username = $config->{$os}->{bundle_id};
+            my $password = $config->{$os}->{apikey};
+            my $carnival_base_uri = $config->{$os}->{base_uri};
 
-        my $response = $ua->request($request);
-        if ($response->is_success) {
-            my $response_json = decode_json($response->content);
-            $self->logger->info("Raw response: " . Dumper($response_json));
-            $self->logger->info("Notification sent - Success");
-        }
-        else {
-            # Something went wrong.
-            $self->logger->error("Error: " . $response->status_line);
+            my $ua  = LWP::UserAgent->new;
+            my $uri = URI->new($carnival_base_uri . '/notifications');
+
+            my $request_json = encode_json({ notification => $request_hash });
+            my $request = POST($uri, "Content-type" => "application/json", Accept => "application/json", Content => $request_json);
+            $request->authorization_basic($username, $password);
+
+            $self->logger->info("Processing pull request for: " . $os);
+            #$self->logger->debug("Raw request: " . Dumper($request)); 
+            my $response = $ua->request($request);
+            if ($response->is_success) {
+                my $response_json = decode_json($response->content);
+                #$self->logger->debug("Raw response: " . Dumper($response_json));
+                $self->logger->info("Notification sent - Success");
+            }
+            else {
+                # Something went wrong.
+                $self->logger->error("Error: " . $response->status_line);
+            }
         }
     } catch {
         my $err = $self->format_error($_);
