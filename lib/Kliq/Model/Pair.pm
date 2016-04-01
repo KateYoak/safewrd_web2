5281679a0aebe7e1b3bfee1c51fd3140aaf81804
Sachin Sebastian <sachinjsk@gmail.com>
2016-04-01 12:15:06 +0000

    Added flare(), list()

--- lib/Kliq/Model/Pair.pm
+++ lib/Kliq/Model/Pair.pm
@@ -63,6 +63,45 @@ sub pair {
     return 0;
 }
 
+sub list {
+    my ($self, $data) = @_;
+
+    my @list;
+    if ($data->{parent_device_id} or $data->{child_device_id}) {
+        my $list = $self->schema->resultset('Pair')->search($data);
+        while (my $pair = $list->next) {
+            push(@list, $pair->get_columns);
+        }
+
+        return \@list;
+    }
+
+    return 0;
+}
+
+
+sub flare {
+    my ($self, $data) = @_;
+
+    if ($data->{code} and $data->{parent_device_id} and $data->{parent_user_id}) {
+        my $is_code_present = $self->schema->resultset('Pair')->find({
+                code => $data->{code}
+            });
+        if($is_code_present) {
+            # We have the code, now lets update the pair
+            $is_code_present->title($data->{title});
+            $is_code_present->parent_device_id($data->{parent_device_id});
+            $is_code_present->parent_user_id($data->{parent_user_id});
+
+            $is_code_present->update();
+
+            return $is_code_present->id;
+        }
+    }
+
+    return 0;
+}
+
 __PACKAGE__->meta->make_immutable;
 
 1;
