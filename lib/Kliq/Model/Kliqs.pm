e69fee9f81da424c2e009120873b4f712329814e
Sachin Sebastian <sachinjsk@gmail.com>
2016-07-04 08:39:44 -0400

    Send email on Kliq creation to those users who hasnt installed the app

--- lib/Kliq/Model/Kliqs.pm
+++ lib/Kliq/Model/Kliqs.pm
@@ -6,13 +6,27 @@ use MooseX::StrictConstructor;
 use Try::Tiny;
 use Data::Dumper;
 use DateTime;
-use JSON ();
+use JSON;
 extends 'Kliq::Model::Base';
 
 sub table { 'Kliq' }
 #sub path { 'kliqs' }
 sub method { 'kliqs' }
 
+around 'create' => sub {
+    my ($orig, $self, $params) = @_;
+
+    my $res = $self->$orig($params);
+
+    # Invite users to the app for those users who doesn't have the app installed
+#    if ($res->{is_emergency}) {
+        $self->redis->rpush(notifyKliq => to_json({
+            kliq  => $res->{id},
+        }));
+#    }
+
+    return $res;
+};
 
 __PACKAGE__->meta->make_immutable;
 
