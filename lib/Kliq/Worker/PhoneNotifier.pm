b1a99cf3e49e9f466566220763ca4fc8b3807a51
Sachin Sebastian <sachinjsk@gmail.com>
2016-07-22 19:29:54 -0400

    Send push notification to tranzmt and flare app for all emergency push notifications

--- lib/Kliq/Worker/PhoneNotifier.pm
+++ lib/Kliq/Worker/PhoneNotifier.pm
@@ -36,33 +36,41 @@ sub work {
         die "Missing 'type' param in request" if (!$request_hash->{type});
         die "Missing 'carnival_payload' param in request" if (!$request_hash->{carnival_payload});
 
-        for my $os ('ios', 'android') {
-            my $username = $config->{$os}->{bundle_id};
-            my $password = $config->{$os}->{apikey};
-            my $carnival_base_uri = $config->{$os}->{base_uri};
-
-            my $ua  = LWP::UserAgent->new;
-            my $end_point = "/notifications";
-            if ($request_hash->{type} eq 'in-app') {
-                $end_point = "/messages";
+        for my $app ('tranzmt', 'flare') {
+            my $action = $request_hash->{carnival_payload}->{notification}->{payload}->{action};
+            if ($app eq 'flare' && $action eq 'live_event') {
+                # Do not send live event notifications to Flare app
+                next;
             }
-            my $uri = URI->new($carnival_base_uri . $end_point);
 
-            my $request_json = encode_json($request_hash->{carnival_payload});
-            my $request = POST($uri, "Content-type" => "application/json", Accept => "application/json", Content => $request_json);
-            $request->authorization_basic($username, $password);
+            for my $os ('ios', 'android') {
+                my $username = $config->{$app}->{$os}->{bundle_id};
+                my $password = $config->{$app}->{$os}->{apikey};
+                my $carnival_base_uri = $config->{$app}->{$os}->{base_uri};
 
-            $self->logger->info("Processing pull request for: " . $os);
-            $self->logger->debug("Raw request: " . Dumper($request)); 
-            my $response = $ua->request($request);
-            if ($response->is_success) {
-                my $response_json = decode_json($response->content);
-                $self->logger->debug("Raw response: " . Dumper($response_json));
-                $self->logger->info("Notification sent - Success");
-            }
-            else {
-                # Something went wrong.
-                $self->logger->error("Error: " . $response->status_line);
+                my $ua  = LWP::UserAgent->new;
+                my $end_point = "/notifications";
+                if ($request_hash->{type} eq 'in-app') {
+                    $end_point = "/messages";
+                }
+                my $uri = URI->new($carnival_base_uri . $end_point);
+
+                my $request_json = encode_json($request_hash->{carnival_payload});
+                my $request = POST($uri, "Content-type" => "application/json", Accept => "application/json", Content => $request_json);
+                $request->authorization_basic($username, $password);
+
+                $self->logger->info("Processing pull request for: " . $os);
+                $self->logger->debug("Raw request: " . Dumper($request)); 
+                my $response = $ua->request($request);
+                if ($response->is_success) {
+                    my $response_json = decode_json($response->content);
+                    $self->logger->debug("Raw response: " . Dumper($response_json));
+                    $self->logger->info("Notification sent - Success");
+                }
+                else {
+                    # Something went wrong.
+                    $self->logger->error("Error: " . $response->status_line);
+                }
             }
         }
     } catch {
