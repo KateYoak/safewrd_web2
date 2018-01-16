3b1532cfc6f81580de715bfbee22c03873b32fcd
Gabriel Andrade <gabiruh@gmail.com>
2018-01-16 16:40:10 -0300

    passing the streaming URL

--- lib/Kliq/Model/Events.pm
+++ lib/Kliq/Model/Events.pm
@@ -78,7 +78,7 @@ sub _call_drone {
       {
         access_token     => $drone->access_token,
         vehicle_id       => $drone->vehicle_id,
-        mission_wait     => 5,
+        mission_wait     => 15,
         mission_location => {lat => $lat + 0, lng => $lng + 0,},
         home_location    => {
           lat => $drone->get_column('lat') + 0,
@@ -87,7 +87,7 @@ sub _call_drone {
         "alt"       => 5,
         "wait_time" => 10,
 
-#	  stream_url  => $self->rtmp_url
+	stream_url  => $event->rtmp_url
 
         # token => $smart_contract_token
       }
